dbbb676f50156b2873e5195c27b4a59f2c7008d9
Carl Pillot <carl.pillot@gmail.com>
2014-07-11 19:00:01 -0400

    Modify config to use environment variables so app will run on Heroku.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
-var config = require('./config');
+//var config = require('./config');
 var playsApp = require('radio-plays');
 var app = express();
+
 var plays = playsApp({
 	source: 'Spinitron',
-	spinitron: config.spinitron
+	spinitron: {
+		station: process.env.SPIN_STATION,
+		userid: process.env.SPIN_USERID,
+		secret: process.env.SPIN_SECRET
+	}
 });
 
 app.get('/nowplaying', function(req, res){
@@ -15,7 +20,9 @@ app.get('/tenmostrecent', function(req, res){
 	res.send(plays.tenMostRecent());
 });
 
-var server = app.listen(3000, function() {
+var port = Number(process.env.PORT || 3000);
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
-	console.log(plays.nowPlaying());
+	plays.nowPlaying();
+	plays.tenMostRecent();
 });
