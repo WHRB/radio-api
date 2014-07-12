f1a94df646e9db5f467ae34a99c804aa500be458
Carl Pillot <carl.pillot@gmail.com>
2014-07-12 00:07:27 -0400

    Updated logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 //var config = require('./config');
 var playsApp = require('radio-plays');
+var logfmt = require("logfmt");
 var app = express();
 
 var plays = playsApp({
@@ -12,17 +13,17 @@ var plays = playsApp({
 	}
 });
 
+app.use(logfmt.requestLogger());
+
 app.get('/nowplaying', function(req, res){
-	res.send(plays.nowPlaying());
+	res.send(plays.recentPlays(1)[0]);
 });
 
-app.get('/tenmostrecent', function(req, res){
-	res.send(plays.tenMostRecent());
+app.get('/recentplays/:num', function(req, res){
+	res.send(plays.recentPlays(Number(req.params.num)));
 });
 
 var port = Number(process.env.PORT || 3000);
 var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
-	plays.nowPlaying();
-	plays.tenMostRecent();
 });
