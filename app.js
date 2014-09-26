0eadbd10012b1abacd6ae5e386f190a0f3da5a94
Carl Pillot <carl.pillot@gmail.com>
2014-09-26 12:25:25 -0700

    Parse schedule and add current show

--- app.js
+++ app.js
@@ -16,29 +16,68 @@ var plays = playsApp({
 	maxAge: Number(process.env.CACHE_TIME || 15000)
 });
 
-var schedule = {};
+var schedule = {
+	timestamp: 0,
+	events: {},
+	current: {}
+};
+
+var isStale = function(timestamp) {
+	diff = new Date() - timestamp;
+	if (diff > 15000) {
+		return true;
+	}
+	return false;
+}
+
+var processGCalV2 = function (response) {
+	var events = response.feed.entry;
+	var newEvents = [];
+	var now = moment();
+	for (i in events) {
+		var e = {
+			title: events[i].title.$t,
+			content: events[i].content.$t,
+			startTime: events[i].gd$when[0].startTime,
+			endTime: events[i].gd$when[0].endTime
+		}
+
+		var startMoment = moment(e.startTime);
+		var endMoment = moment(e.endTime);
+
+		if (startMoment.isBefore(now) && now.isBefore(endMoment)) {
+			schedule.current = e;
+		}
+		newEvents.push(e);
+	}
+	schedule.events = newEvents;
+	schedule.timestamp = new Date();
+}
 
 var get_schedule = function() {
-	var url = 'http://www.google.com/calendar/feeds/'
-			  + process.env.SCHEDULE_ID
-			  + '/public/full?orderby=starttime&sortorder=ascending'
-			  + '&singleevents=true&futureevents=true&alt=json';
-			  //+ encodeURI('&start-min='+moment().format("YYYY-MM-DDTHH:mm:ssZ"));
-			  //console.log(url);
-			  http.get(url, function(res) {
-				var body = '';
+	if (isStale(schedule.timestamp)) {
+		console.log('Fetching new calendar');
+		var url = 'http://www.google.com/calendar/feeds/'
+				  + process.env.SCHEDULE_ID
+				  + '/public/full?orderby=starttime&sortorder=ascending'
+				  + '&singleevents=true&futureevents=true&alt=json';
+				  //+ encodeURI('&start-min='+moment().format("YYYY-MM-DDTHH:mm:ssZ"));
+				  //console.log(url);
+		http.get(url, function(res) {
+			var body = '';
 
-				res.on('data', function(chunk) {
-					body += chunk;
-				});
-
-				res.on('end', function() {
-					var response = JSON.parse(body);
-					schedule = response;
-				});
-			}).on('error', function(e) {
-				  console.log("Got error: ", e);
+			res.on('data', function(chunk) {
+				body += chunk;
 			});
+
+			res.on('end', function() {
+				processGCalV2(JSON.parse(body));
+
+			});
+		}).on('error', function(e) {
+			  console.log("Got error: ", e);
+		});
+	}
 }
 
 var streams = streamManager({});
@@ -59,7 +98,12 @@ app.get('/streams', function(req, res){
 
 app.get('/schedule', function(req, res){
 	get_schedule();
-	res.jsonp(schedule);
+	res.jsonp(schedule.events);
+});
+
+app.get('/schedule/current', function(req, res){
+	get_schedule();
+	res.jsonp(schedule.current);
 });
 
 var port = Number(process.env.PORT || 3000);
