3b0095315d9e6cff1774bdc1f981c61b66221473
Carl Pillot <carl.pillot@gmail.com>
2014-11-17 23:14:15 -0800

    Whitespace and tabs

--- app.js
+++ app.js
@@ -10,83 +10,83 @@ var pg = require('pg');
 var google_tokens = null;
 
 var plays = playsApp({
-	source: 'Spinitron',
-	spinitron: {
-		station: process.env.SPIN_STATION,
-		userid: process.env.SPIN_USERID,
-		secret: process.env.SPIN_SECRET
-	},
-	maxAge: Number(process.env.CACHE_TIME || 15000)
+    source: 'Spinitron',
+    spinitron: {
+        station: process.env.SPIN_STATION,
+        userid: process.env.SPIN_USERID,
+        secret: process.env.SPIN_SECRET
+    },
+    maxAge: Number(process.env.CACHE_TIME || 15000)
 });
 
 var OAuth2 = googleapis.auth.OAuth2;
 var auth = new OAuth2(
-	process.env.GOOGLE_CLIENT_ID,
-	process.env.GOOGLE_CLIENT_SECRET,
-	process.env.GOOGLE_REDIRECT_URL
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URL
 );
 googleapis.options({ auth: auth });
 var calendar = googleapis.calendar('v3');
 
 var schedule = {
-	timestamp: 0,
-	events: {},
-	current: {},
-	v3: {}
+    timestamp: 0,
+    events: {},
+    current: {},
+    v3: {}
 };
 
 var isStale = function(timestamp) {
-	diff = new Date() - timestamp;
-	if (diff > 15000) {
-		return true;
-	}
-	return false;
+    diff = new Date() - timestamp;
+    if (diff > 15000) {
+        return true;
+    }
+    return false;
 }
 
 var processGCalV3 = function (response) {
-	var events = response.items;
-	var newEvents = [];
-	var now = moment();
-	for (i in events) {
-		var e = {
-			title: events[i].summary,
-			content: '',
-			startTime: events[i].start.dateTime,
-			endTime: events[i].end.dateTime
-		}
+    var events = response.items;
+    var newEvents = [];
+    var now = moment();
+    for (i in events) {
+        var e = {
+            title: events[i].summary,
+            content: '',
+            startTime: events[i].start.dateTime,
+            endTime: events[i].end.dateTime
+        }
 
-		var startMoment = moment(e.startTime);
-		var endMoment = moment(e.endTime);
+        var startMoment = moment(e.startTime);
+        var endMoment = moment(e.endTime);
 
-		if (startMoment.isBefore(now) && now.isBefore(endMoment)) {
-			schedule.current = e;
-		}
-		newEvents.push(e);
-	}
-	schedule.events = newEvents;
-	schedule.timestamp = new Date();
+        if (startMoment.isBefore(now) && now.isBefore(endMoment)) {
+            schedule.current = e;
+        }
+        newEvents.push(e);
+    }
+    schedule.events = newEvents;
+    schedule.timestamp = new Date();
 }
 
 var getSchedule = function() {
-	if (isStale(schedule.timestamp)) {
-		console.log('Fetching new calendar');
+    if (isStale(schedule.timestamp)) {
+        console.log('Fetching new calendar');
 
-		schedule.events = [];
-		schedule.timestamp = new Date();
-		calendar.events.list({
-			'calendarId': process.env.SCHEDULE_ID,
-			'singleEvents': true,
-			'orderBy': 'startTime',
-			'timeMin': schedule.timestamp.toISOString()
-			//'timeMin': schedule.timestamp
-		}, function (err, response) {
-			if (!err) {
-				processGCalV3(response);
-			} else {
-				console.log("Got calendar error: ", err);
-			}
-		});
-	}
+        schedule.events = [];
+        schedule.timestamp = new Date();
+        calendar.events.list({
+            'calendarId': process.env.SCHEDULE_ID,
+            'singleEvents': true,
+            'orderBy': 'startTime',
+            'timeMin': schedule.timestamp.toISOString()
+            //'timeMin': schedule.timestamp
+        }, function (err, response) {
+            if (!err) {
+                processGCalV3(response);
+            } else {
+                console.log("Got calendar error: ", err);
+            }
+        });
+    }
 }
 
 var streams = streamManager({});
@@ -98,98 +98,97 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
     return console.error('could not connect to postgres', err);
   }
   client.query('CREATE TABLE IF NOT EXISTS tokens (\
-  	  ID SERIAL PRIMARY KEY,\
-  	  REFRESH TEXT NOT NULL,\
-  	  ACCESS TEXT NOT NULL)', function(err, result) {
+      ID SERIAL PRIMARY KEY,\
+      REFRESH TEXT NOT NULL,\
+      ACCESS TEXT NOT NULL)', function(err, result) {
       if(err) {
-      	  return console.error('error running query', err);
+          return console.error('error running query', err);
       }
       client.query('SELECT * FROM tokens', function(err, result) {
-		  if(err) {
-		    return console.error('error running token query', err);
-		  }
-		  if (result.rows.length > 0) {
-			  google_tokens = {
-				  access_token: result.rows[0].access,
-				  refresh_token: result.rows[0].refresh
-			  }
-			  console.log(google_tokens);
-			  auth.setCredentials(google_tokens);
-			  getSchedule();
-		  }
-		  client.end();
-	  });
+          if(err) {
+            return console.error('error running token query', err);
+          }
+          if (result.rows.length > 0) {
+              google_tokens = {
+                  access_token: result.rows[0].access,
+                  refresh_token: result.rows[0].refresh
+              }
+              console.log(google_tokens);
+              auth.setCredentials(google_tokens);
+              getSchedule();
+          }
+          client.end();
+      });
   });
 });
 
 
 app.get('/nowplaying', function(req, res){
-	res.jsonp(plays.recentPlays(1)[0]);
+    res.jsonp(plays.recentPlays(1)[0]);
 });
 
 app.get('/recentplays/:num', function(req, res){
-	res.jsonp(plays.recentPlays(Number(req.params.num)));
+    res.jsonp(plays.recentPlays(Number(req.params.num)));
 });
 
 app.get('/streams', function(req, res){
-	res.jsonp(streams.streams());
+    res.jsonp(streams.streams());
 });
 
 app.get('/schedule', function(req, res){
-	getSchedule();
-	res.jsonp(schedule);
+    getSchedule();
+    res.jsonp(schedule);
 });
 
 app.get('/api_auth', function(req, res){
-	if (google_tokens == null) {
-		var url = auth.generateAuthUrl({
-		  access_type: 'offline',
-		  approval_prompt: 'force',
-		  scope: process.env.GOOGLE_SCOPE
-		});
-		res.redirect(url);
-	} else {
-		// We've already authenticated.
-		res.redirect('/schedule');
-	}
+    if (google_tokens == null) {
+        var url = auth.generateAuthUrl({
+          access_type: 'offline',
+          approval_prompt: 'force',
+          scope: process.env.GOOGLE_SCOPE
+        });
+        res.redirect(url);
+    } else {
+        // We've already authenticated.
+        res.redirect('/schedule');
+    }
 });
 
 app.get(process.env.GOOGLE_REDIRECT_PATH, function(req, res){
-	auth.getToken(req.query.code, function(err, tokens) {
+    auth.getToken(req.query.code, function(err, tokens) {
 
-		if(!err) {
-			auth.setCredentials(tokens);
-			google_tokens = tokens;
-			console.log(tokens);
+        if(!err) {
+            auth.setCredentials(tokens);
+            google_tokens = tokens;
+            console.log(tokens);
 
-			// Save tokens to database
-			pg.connect(process.env.DATABASE_URL, function(err, client) {
-				if(err) {
-					return console.error('could not connect to postgres', err);
-				}
+            // Save tokens to database
+            pg.connect(process.env.DATABASE_URL, function(err, client) {
+                if(err) {
+                    return console.error('could not connect to postgres', err);
+                }
 
-				client.query('TRUNCATE TABLE tokens', function(err, result) {
-					if(err) {
-						return console.error('error truncating token table', err);
-					}
-					client.query('INSERT INTO tokens (ACCESS, REFRESH) VALUES($1, $2)',
-					[tokens.access_token, tokens.refresh_token], function(err, result) {
-						if(err) {
-							return console.error('error saving tokens', err);
-						}
-						client.end();
-					});
-				});
+                client.query('TRUNCATE TABLE tokens', function(err, result) {
+                    if(err) {
+                        return console.error('error truncating token table', err);
+                    }
+                    client.query('INSERT INTO tokens (ACCESS, REFRESH) VALUES($1, $2)',
+                    [tokens.access_token, tokens.refresh_token], function(err, result) {
+                        if(err) {
+                            return console.error('error saving tokens', err);
+                        }
+                        client.end();
+                    });
+                });
 
-			});
+            });
 
-			res.redirect('/schedule');
-		} else {
-			console.error('error getting auth tokens', err);
-		}
-	});
+            res.redirect('/schedule');
+        } else {
+            console.error('error getting auth tokens', err);
+        }
+    });
 });
-
 
 var port = Number(process.env.PORT || 3000);
 var server = app.listen(port, function() {
