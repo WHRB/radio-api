824739c89ceda27c01b3a5083280b30d63480f2e
Carl Pillot <carl.pillot@gmail.com>
2014-11-17 20:05:28 -0800

    Try again.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var plays = playsApp({
 	maxAge: Number(process.env.CACHE_TIME || 15000)
 });
 
-var OAuth2 = google.auth.OAuth2;
+var OAuth2 = googleapis.auth.OAuth2;
 var auth = new OAuth2(
 	process.env.GOOGLE_CLIENT_ID,
 	process.env.GOOGLE_CLIENT_SECRET,
