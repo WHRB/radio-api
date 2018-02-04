188825c86ce8886d525302f71c04796f082e17ac
Carl Pillot <carl.pillot@gmail.com>
2018-02-04 13:36:46 -0800

    fix: add base route.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+'use strict';
+
 var makeServer = require('./lib/server');
 
 var port = Number(process.env.PORT || 3000);
 
-var server = makeServer(port ,function() {
+var server = makeServer(port, function() {
   console.log('Listening on port %d', server.address().port);
 });
