70e524fff7fcc85a18660a027b1bb35a63fa2123
Carl Pillot <contact@carlpillot.com>
2025-10-22 12:07:15 -0400

    Update chai deps

--- test/spinitronSourceSpec.test.js
+++ test/spinitronSourceSpec.test.js
@@ -1,43 +1,43 @@
 /* eslint-env mocha */
 
-"use strict";
+'use strict';
 
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
-const nock = require("nock");
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised').default;
+const nock = require('nock');
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
-const spinitronSource = require("../lib/radio-plays/spinitronSource");
+const spinitronSource = require('../lib/radio-plays/spinitronSource');
 
-const userid = "this";
-const secret = "is a";
-const station = "test";
+const userid = 'this';
+const secret = 'is a';
+const station = 'test';
 
 const testSongs = [
-  { SongName: "testSong", ShowID: "123" },
-  { SongName: "testSong2", ShowID: "123" },
-  { SongName: "testSong3", ShowID: "123" },
-  { SongName: "testSong4", ShowID: "123" },
-  { SongName: "testSong5", ShowID: "456" }
+  { SongName: 'testSong', ShowID: '123' },
+  { SongName: 'testSong2', ShowID: '123' },
+  { SongName: 'testSong3', ShowID: '123' },
+  { SongName: 'testSong4', ShowID: '123' },
+  { SongName: 'testSong5', ShowID: '456' },
 ];
-const testShow = { ShowName: "testShow", ShowID: "123" };
+const testShow = { ShowName: 'testShow', ShowID: '123' };
 
 beforeEach(() => {
-  nock("http://spinitron.com")
-    .get("/public/spinpapi.php")
+  nock('http://spinitron.com')
+    .get('/public/spinpapi.php')
     .query(true)
     .reply(200, {
       success: true,
-      results: testSongs
+      results: testSongs,
     });
 
-  nock("http://spinitron.com")
-    .get("/public/spinpapi.php")
+  nock('http://spinitron.com')
+    .get('/public/spinpapi.php')
     .query(true)
     .reply(200, {
       success: true,
-      results: testShow
+      results: testShow,
     });
 });
 
@@ -45,30 +45,30 @@ afterEach(() => {
   nock.cleanAll();
 });
 
-describe("spinitronSource", () => {
-  it("should throw an error if userid, secret, and station are not specified", () => {
+describe('spinitronSource', () => {
+  it('should throw an error if userid, secret, and station are not specified', () => {
     expect(spinitronSource.bind(spinitronSource, {})).to.throw(
-      "Credentials must be supplied"
+      'Credentials must be supplied'
     );
-    expect(spinitronSource.bind(spinitronSource, { userid: "foo" })).to.throw(
-      "Credentials must be supplied"
+    expect(spinitronSource.bind(spinitronSource, { userid: 'foo' })).to.throw(
+      'Credentials must be supplied'
     );
-    expect(spinitronSource.bind(spinitronSource, { secret: "bar" })).to.throw(
-      "Credentials must be supplied"
+    expect(spinitronSource.bind(spinitronSource, { secret: 'bar' })).to.throw(
+      'Credentials must be supplied'
     );
-    expect(spinitronSource.bind(spinitronSource, { station: "baz" })).to.throw(
-      "Credentials must be supplied"
+    expect(spinitronSource.bind(spinitronSource, { station: 'baz' })).to.throw(
+      'Credentials must be supplied'
     );
     expect(
       spinitronSource.bind(spinitronSource, {
-        userid: "",
-        secret: "",
-        station: ""
+        userid: '',
+        secret: '',
+        station: '',
       })
-    ).to.throw("Credentials must be supplied");
+    ).to.throw('Credentials must be supplied');
   });
 
-  describe("#getMostCurrentPlay()", () => {
+  describe('#getMostCurrentPlay()', () => {
     let source;
 
     before(() => {
@@ -76,7 +76,7 @@ describe("spinitronSource", () => {
         // Get keys from environment
         userid,
         secret,
-        station
+        station,
       });
     });
 
@@ -84,34 +84,32 @@ describe("spinitronSource", () => {
       source.flushCache();
     });
 
-    it("should return an object with play data", () =>
+    it('should return an object with play data', () =>
       expect(source.getMostCurrentPlay()).to.eventually.have.property(
-        "SongName",
-        "testSong"
+        'SongName',
+        'testSong'
       ));
 
-    it("should return an object with show info", () =>
+    it('should return an object with show info', () =>
       expect(source.getMostCurrentPlay()).to.eventually.have.property(
-        "ShowInfo"
+        'ShowInfo'
       ));
 
-    it("should reject with an error if Spinitron API is unavailable", () => {
+    it('should reject with an error if Spinitron API is unavailable', () => {
       nock.cleanAll();
-      nock("http://spinitron.com")
-        .get("/public/spinpapi.php")
-        .reply(500);
+      nock('http://spinitron.com').get('/public/spinpapi.php').reply(500);
       return expect(source.getMostCurrentPlay()).to.eventually.be.rejected;
     });
   });
 
-  describe("#getMostCurrentShowInfo()", () => {
+  describe('#getMostCurrentShowInfo()', () => {
     let source;
 
     before(() => {
       source = spinitronSource({
         userid,
         secret,
-        station
+        station,
       });
     });
 
@@ -119,20 +117,20 @@ describe("spinitronSource", () => {
       source.flushCache();
     });
 
-    it("should return an object with current show data", () =>
+    it('should return an object with current show data', () =>
       expect(source.getMostCurrentShowInfo()).to.eventually.deep.equal(
         testShow
       ));
   });
 
-  describe("#getRecent()", () => {
+  describe('#getRecent()', () => {
     let source;
 
     before(() => {
       source = spinitronSource({
         userid,
         secret,
-        station
+        station,
       });
     });
 
@@ -140,7 +138,7 @@ describe("spinitronSource", () => {
       source.flushCache();
     });
 
-    it("should return an array", () =>
+    it('should return an array', () =>
       expect(source.getRecent(5)).to.eventually.be.instanceof(Array));
   });
 });
