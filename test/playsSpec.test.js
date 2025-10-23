180d6bafcd20dbcdb5ff71b983b81350569f168b
Carl Pillot <2014739+cpill0789@users.noreply.github.com>
2025-10-22 22:27:11 -0400

    Update eslint and fix some of the issues identified

--- test/playsSpec.test.js
+++ test/playsSpec.test.js
@@ -1,50 +1,44 @@
-"use strict";
+delete require.cache[require.resolve('../lib/radio-plays/plays')];
 
-delete require.cache[require.resolve("../lib/radio-plays/plays")];
+const expect = require('chai').expect;
+const RadioPlays = require('../lib/radio-plays/plays');
 
-const expect = require("chai").expect;
-const RadioPlays = require("../lib/radio-plays/plays");
+const token = 'this';
 
-const userid = "this";
-const secret = "is a";
-const station = "test";
-
-describe("RadioPlays", () => {
-  describe("#setSource()", () => {
-    it("should throw an error if source type is not defined", () => {
+describe('RadioPlays', () => {
+  describe('#setSource()', () => {
+    it('should throw an error if source type is not defined', () => {
       expect(
-        RadioPlays.setSource.bind(RadioPlays.setSource, "foo", {})
-      ).to.throw("SourceType not defined.");
+        RadioPlays.setSource.bind(RadioPlays.setSource, 'foo', {})
+      ).to.throw('SourceType not defined.');
     });
 
-    it("should successfully set an available source", () => {
+    it('should successfully set an available source', () => {
       expect(
-        RadioPlays.setSource.bind(RadioPlays.setSource, "Spinitron", {
-          userid,
-          secret,
-          station
+        RadioPlays.setSource.bind(RadioPlays.setSource, 'SpinitronV2', {
+          token,
         })
       ).to.be.ok;
     });
   });
 
-  describe("#getCurrent()", () => {
+  describe('#getCurrent()', () => {
     it("should throw an error if source hasn't been set", () => {
       expect(
         RadioPlays.getCurrent.bind(RadioPlays.getCurrent, () => {})
-      ).to.throw("No source has been set.");
+      ).to.throw('No source has been set.');
     });
 
-    it("should delegate call to specified source");
+    it('should delegate call to specified source');
   });
 
-  describe("#recentPlays()", () => {
+  describe('#recentPlays()', () => {
     it("should throw an error if source hasn't been set", () => {
       expect(
         RadioPlays.recentPlays.bind(RadioPlays.recentPlays, 5, () => {})
-      ).to.throw("No source has been set.");
+      ).to.throw('No source has been set.');
     });
 
-    it("should delegate call to specified source");
+    it('should delegate call to specified source');
   });
 });
